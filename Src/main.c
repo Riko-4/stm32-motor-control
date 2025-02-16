428d2caab86f1c816f5e4cac007eea97cb7d91de
Riko-4 <rigvedak04@gmail.com>
2025-02-16 23:56:10 +0100

    Ultrasonic sensor motor control main.c

--- Src/main.c
+++ Src/main.c
@@ -1,414 +1,169 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+  /* USER CODE BEGIN Header */
+  /**
+    ******************************************************************************
+    * @file           : main.c
+    * @brief          : Main program body
+    ******************************************************************************
+    */
+  /* USER CODE END Header */
+  /* Includes ------------------------------------------------------------------*/
+  #include "main.h"
+  #include <stdio.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+  /* Private variables ---------------------------------------------------------*/
+  TIM_HandleTypeDef htim1;
+  TIM_HandleTypeDef htim2;
+  TIM_HandleTypeDef htim3;
+  TIM_HandleTypeDef htim4; // Timer for Ultrasonic Sensor
 
-/* USER CODE END Includes */
+  /* USER CODE BEGIN PV */
+  uint32_t IC_Val1 = 0;
+  uint32_t IC_Val2 = 0;
+  uint8_t Is_First_Captured = 0;
+  uint32_t Distance = 0;
+  /* USER CODE END PV */
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+  /* Private function prototypes -----------------------------------------------*/
+  void SystemClock_Config(void);
+  static void MX_GPIO_Init(void);
+  static void MX_TIM1_Init(void);
+  static void MX_TIM2_Init(void);
+  static void MX_TIM3_Init(void);
+  static void MX_TIM4_Init(void); // Timer for Ultrasonic Sensor
+  void delay(uint16_t time);
+  void HCSR04_Read(void);
 
-/* USER CODE END PTD */
+  /* USER CODE BEGIN 0 */
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-TIM_HandleTypeDef htim1;
-TIM_HandleTypeDef htim2;
-TIM_HandleTypeDef htim3;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_TIM1_Init(void);
-static void MX_TIM2_Init(void);
-static void MX_TIM3_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_TIM1_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
-  /* USER CODE BEGIN 2 */
-  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
-
-  // Set initial duty cycle (0-999 for 0% to 100%)
-  uint16_t speed_motor1 = 500; // 50% duty cycle
-  uint16_t speed_motor2 = 750; // 75% duty cycle
-  uint16_t speed_motor3 = 250; // 25% duty cycle
-
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, speed_motor1);
-  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, speed_motor2);
-  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, speed_motor3);
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	  //  Forward Motion (PWM Ramps Up)
-	      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET); // Motor 1 Forward
-	      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET); // Motor 2 Forward
-	      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_SET); // Motor 3 Forward
-
-	      for (uint16_t duty = 0; duty <= 999; duty += 50)
-	      {
-	          __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, duty);
-	          __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, duty);
-	          __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, duty);
-	          HAL_Delay(100);
-	      }
-
-	      HAL_Delay(1000); // Maintain at max speed for a while
-
-	      //  Reverse Motion (PWM Ramps Down)
-	      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET); // Motor 1 Reverse
-	      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET); // Motor 2 Reverse
-	      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_RESET); // Motor 3 Reverse
-
-	      for (uint16_t duty = 999; duty > 0; duty -= 50)
-	      {
-	          __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, duty);
-	          __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, duty);
-	          __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, duty);
-	          HAL_Delay(100);
-	      }
-
-	      HAL_Delay(1000); // Maintain at min speed before switching back
-	    }
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
+  /* Delay Function using Timer */
+  void delay(uint16_t time) {
+      __HAL_TIM_SET_COUNTER(&htim4, 0);
+      while (__HAL_TIM_GET_COUNTER(&htim4) < time);
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /* Ultrasonic Sensor Measurement */
+  void HCSR04_Read(void) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);  // Trigger HIGH
+      delay(10); // 10us delay
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET); // Trigger LOW
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
+      __HAL_TIM_ENABLE_IT(&htim4, TIM_IT_CC1);  // Enable interrupt to capture Echo pulse
   }
-}
 
-/**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM1_Init(void)
-{
+  /* Capture Callback Function for Timer Interrupt */
+  void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
+      if (htim->Instance == TIM4) {  // If the interrupt is triggered by TIM4
+          if (Is_First_Captured == 0) {
+              IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+              Is_First_Captured = 1;
+              __HAL_TIM_SET_COUNTER(htim, 0);
+          } else {
+              IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+              __HAL_TIM_SET_COUNTER(htim, 0);
 
-  /* USER CODE BEGIN TIM1_Init 0 */
-
-  /* USER CODE END TIM1_Init 0 */
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
-
-  /* USER CODE BEGIN TIM1_Init 1 */
-
-  /* USER CODE END TIM1_Init 1 */
-  htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 71;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 999;
-  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
-  {
-    Error_Handler();
+              if (IC_Val2 > IC_Val1) {
+                  Distance = (IC_Val2 - IC_Val1) * 0.034 / 2;
+              }
+              Is_First_Captured = 0;
+              __HAL_TIM_DISABLE_IT(&htim4, TIM_IT_CC1);
+          }
+      }
   }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
+
+  /* USER CODE END 0 */
+
+  /**
+    * @brief  The application entry point.
+    */
+  int main(void) {
+      /* MCU Configuration */
+      HAL_Init();
+      SystemClock_Config();
+
+      /* Initialize all configured peripherals */
+      MX_GPIO_Init();
+      MX_TIM1_Init();
+      MX_TIM2_Init();
+      MX_TIM3_Init();
+      MX_TIM4_Init();
+
+      HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+      HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+      HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+      HAL_TIM_IC_Start_IT(&htim4, TIM_CHANNEL_1); // Start input capture interrupt for ultrasonic sensor
+
+      uint16_t speed_motor1 = 500;
+      uint16_t speed_motor2 = 750;
+      uint16_t speed_motor3 = 250;
+
+      /* Infinite loop */
+      while (1) {
+          HCSR04_Read(); // Measure distance
+
+          if (Distance > 10) { // If obstacle is farther than 10cm
+              HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+              HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
+              HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_SET);
+
+              for (uint16_t duty = 0; duty <= 999; duty += 50) {
+                  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, duty);
+                  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, duty);
+                  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, duty);
+                  HAL_Delay(100);
+              }
+          } else { // If obstacle detected within 10cm, stop motors
+              __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
+              __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+              __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 0);
+          }
+
+          HAL_Delay(1000);
+      }
   }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
-  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
+
+  /**
+    * @brief TIM4 Initialization Function (For Ultrasonic Sensor Timing)
+    */
+  static void MX_TIM4_Init(void) {
+      TIM_IC_InitTypeDef sConfigIC = {0};
+
+      htim4.Instance = TIM4;
+      htim4.Init.Prescaler = 71;
+      htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+      htim4.Init.Period = 65535;
+      htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+      htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+      if (HAL_TIM_IC_Init(&htim4) != HAL_OK) {
+          Error_Handler();
+      }
+
+      sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+      sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+      sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+      sConfigIC.ICFilter = 0;
+      if (HAL_TIM_IC_ConfigChannel(&htim4, &sConfigIC, TIM_CHANNEL_1) != HAL_OK) {
+          Error_Handler();
+      }
   }
-  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
-  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
-  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-  sBreakDeadTimeConfig.DeadTime = 0;
-  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
-  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
-  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
-  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
-  {
-    Error_Handler();
+
+  /**
+    * @brief GPIO Initialization Function (Added Ultrasonic Sensor GPIOs)
+    */
+  static void MX_GPIO_Init(void) {
+      GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+      __HAL_RCC_GPIOA_CLK_ENABLE();
+
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_8, GPIO_PIN_RESET);
+
+      GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_8;
+      GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+      GPIO_InitStruct.Pull = GPIO_NOPULL;
+      GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+      GPIO_InitStruct.Pin = GPIO_PIN_9; // Echo Pin
+      GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+      GPIO_InitStruct.Pull = GPIO_NOPULL;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   }
-  /* USER CODE BEGIN TIM1_Init 2 */
-
-  /* USER CODE END TIM1_Init 2 */
-  HAL_TIM_MspPostInit(&htim1);
-
-}
-
-/**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM2_Init(void)
-{
-
-  /* USER CODE BEGIN TIM2_Init 0 */
-
-  /* USER CODE END TIM2_Init 0 */
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  /* USER CODE BEGIN TIM2_Init 1 */
-
-  /* USER CODE END TIM2_Init 1 */
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 71;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 999;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM2_Init 2 */
-
-  /* USER CODE END TIM2_Init 2 */
-  HAL_TIM_MspPostInit(&htim2);
-
-}
-
-/**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
-{
-
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 71;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 999;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  /* USER CODE END TIM3_Init 2 */
-  HAL_TIM_MspPostInit(&htim3);
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PA11 PA12 PA13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
